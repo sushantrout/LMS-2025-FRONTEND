a51960766c0e25b9e9184f51eab76680847f3b91
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2025-05-03 10:33:21 +0530

    updated

---
+++ bip-lms-frontend/src/components/model/comment-model.ts
@@ -0,0 +1,6 @@
+export type Comment = {
+    id: string;
+    userId: string;
+    comment: string;
+    parentCommentId: string;
+  };
---
+++ bip-lms-frontend/src/components/model/module-model.ts
@@ -0,0 +1,9 @@
+export type Module = {
+    id: string;
+    name: string;
+    description: string;
+    trainerId: string;
+    sortOrder: number;
+    noOfSessions: number;
+  };
+  
